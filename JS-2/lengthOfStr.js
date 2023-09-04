4141ef00e20d884e4ff84a9288e1b6bb45063e8e
nishant02998 <thenishantsrivastava@gmail.com>
2023-09-04 22:26:20 +0530

    JS-2

---
+++ JS-2/lengthOfStr.js
@@ -0,0 +1,9 @@
+// var str = "abcdefghijklmnopqrstuvwxyz";
+// alert(str.length); 
+
+
+document.getElementById("btn").onclick = function () {
+    var word = prompt("Enter a word");
+    alert(word.length);
+  };
+
---
+++ JS-2/strConcat.js
@@ -0,0 +1,3 @@
+var str1 = "HELLO";
+var str2 = "WORLD";
+alert(str1 + str2);
